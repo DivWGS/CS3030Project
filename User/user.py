655776ff57eb6aa49e6b0ea364821ebff95f6c22
Dillon <ddivich@uccs.edu>
2019-05-11 19:34:21 -0600

    updated user class
    added food preference
    added write user function, writes to file userList.txt

--- User/user.py
+++ User/user.py
@@ -1,9 +1,18 @@
 # This is where the user object will go
 
 class User():
-	def __init__(self, username, email, notificationTime, zipCode):
+	def __init__(self, username, email, notificationTime, zipCode, preference):
 		self.username = username
 		self.email = email
 		self.notificationTime = notificationTime
 		self.zipCode = zipCode
+		self.preference = preference
 		
+	def writeUser(self):
+                f = open("userList.txt", "a+")
+                f.write("Username: " + self.username)
+                f.write("\nEmail: " + self.email)
+                f.write("\nNotification Time: " + self.notificationTime)
+                f.write("\nZipcode: " + self.zipCode)
+                f.write("\nPreferences: " + self.preference + "\n\n")
+                f.close()
